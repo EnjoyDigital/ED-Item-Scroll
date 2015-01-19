8cb5293d25d433af181b6fb3e3117edd349a8b6b
crot4lus <glenn@glenntaylor.co.uk>
2015-01-19 15:11:04 +0000

    Create simpleItemScroll.js

---
+++ simpleItemScroll.js
@@ -0,0 +1,34 @@
+/**
+ * Allows you to show items when they are in the viewport (timeline etc...)
+ */
+$.fn.simpleItemScroll = function(options){
+	'use strict';
+	var settings $.extend({
+		'effect': 'opacity',
+		'speed': 400,
+		'callback': function(){}
+	}, options);
+
+	return this.each(function(){
+		var element = $(this);
+		var rect = $(this)[0].getBoundingClientRect();
+
+		if(rect.top >= 0 && rect.left >= 0 && rect.bottom <= $(window).height() && rect.right <= $(window).width()){
+			if(settings.effect == 'opacity'){
+				$(this).animate({
+					opacity: 1
+				}, settings.speed, 'swing', function(){
+					if(settings.callback){
+						settings.callback.call(this);
+					}
+				});
+			}else if(settings.effect == 'show'){
+				$(this).fadeIn(settings.speed, function(){
+					if(settings.callback){
+						settings.callback.call(this);
+					}
+				});
+			}
+		}
+	});
+};
